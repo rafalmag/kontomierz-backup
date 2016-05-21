943474cf237219e1dab8757af14b8dde07a2b734
Rafal Magda <rafalmag@gmail.com>
2016-05-22 01:11:34 +0200

    exporter can update existing data

--- src/main/groovy/pl/rafalmag/kontomierz/Exporter.groovy
+++ src/main/groovy/pl/rafalmag/kontomierz/Exporter.groovy
@@ -3,9 +3,11 @@ package pl.rafalmag.kontomierz
 import com.mongodb.client.MongoCollection
 import com.mongodb.client.MongoDatabase
 import com.mongodb.client.model.IndexOptions
+import com.mongodb.client.model.UpdateOptions
 import groovy.util.logging.Slf4j
 import org.bson.BsonDocument
 import org.bson.Document
+import org.bson.conversions.Bson
 import pl.rafalmag.kontomierz.apimappings.ApiMapping
 
 import javax.inject.Inject
@@ -26,6 +28,25 @@ class Exporter {
             new Document(it)
         }
         log.debug("$apiMapping.collectionName documents to persist: {}", documents)
-        collection.insertMany(documents)
+        documents.each {
+            Bson query = getQuery(apiMapping, it.get(apiMapping.id))
+            def result = collection.replaceOne(query, it, new UpdateOptions().upsert(true))
+            boolean updated = result.getUpsertedId() == null
+            if (updated) {
+                log.info("Updated existing document with id={}", it.get(apiMapping.id))
+            } else {
+                log.debug("Inserted new document")
+            }
+        }
+    }
+
+    protected Bson getQuery(ApiMapping apiMapping, id) {
+        def query
+        if (id instanceof Number) {
+            query = "{ \$or: [ { \"$apiMapping.id\": $id }, { \"$apiMapping.id\": \"$id\" } ] }"
+        } else {
+            query = "{ \"$apiMapping.id\": \"$id\" }"
+        }
+        BsonDocument.parse(query)
     }
 }
